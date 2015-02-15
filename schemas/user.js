87d09ab7466a404ffbedfc661ea021cb90c910eb
laurent duchaussoy <ryoleryo@gmail.com>
2015-02-15 11:18:43 +0100

    création modèle = schémas User (export + require)

---
+++ schemas/user.js
@@ -0,0 +1,11 @@
+module.exports = function(app) {
+	var mongoose = require("mongoose");
+
+	var userSchema = new mongoose.Schema({
+	  pseudo : { type : String, match: /^[a-zA-Z0-9-_]+$/ },
+	  password : String,
+	  date : { type : Date, default : Date.now }
+	});
+
+	module.exports = mongoose.model('User', userSchema); 
+}
--- server.js
+++ server.js
@@ -4,42 +4,35 @@
     var express  = require('express');
     var app      = express();                               // create our app w/ express
     var mongoose = require('mongoose');                     // mongoose for mongodb
-    var morgan = require('morgan');             // log requests to the console (express4)
-    var http = require('http');
-    var url = require('url');
+    var morgan   = require('morgan');             // log requests to the console (express4)
+    var http     = require('http');
+    var url      = require('url');
     var passport = require('passport')
-    //var passport-local = require('passport-local')
-    var http = require('http');
+        , LocalStrategy = require('passport-local').Strategy;
+    var http     = require('http');
     //var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
     //var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
-    // configuration =================
+    // mongo connect =================
+    mongoose.connect('mongodb://mongoLogin:admin@ds043991.mongolab.com:43991/domo-project-db');     // connect to mongoDB database on modulus.io
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function (callback) {
+        console.log('connection ok calback ->', callback)
+    });
 
+    // models & schemas ======================================================================
+    require('./schemas/schemas')(app);
 
-    //mongoose.connect('mongodb://LduProject:MongoDb59650@ds053080.mongolab.com:53080/projectdb');     // connect to mongoDB database on modulus.io
+    //routes app ======================================
+    require('./app/routes')(app, passport);
     app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
     //app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
     //app.use(bodyParser.json());                                     // parse application/json
     //app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
     //app.use(methodOverride());
-    //console.log('mongo->',mongoose)
-
-    // routes ======================================================================
-
-    //routes app ======================================
-    app.get('/', function(req, res) {
-       res.sendfile('app/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-        console.log('res')
-    });
-
-    app.get('/test', function(req, res) {
-        console.log('test route');
-    });
-
-
-    app.listen(8080);
 
     // listen (start app with node server.js) ======================================
-    //app.listen(8080);
+    app.listen(8080);
 
